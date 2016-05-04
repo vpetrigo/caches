ca0a8a68220f3913e799d8b901cb01845ccfcdc9
Vladimir Petrigo <taenaru@gmail.com>
2016-05-04 13:01:45 +0300

    optimize Put() method for LRU and FIFO caches
    if element with specific key was not found then we check whether that addition
    surpass max_cache_size.
    If it is then we delete element according to the cache algorithm specification
    and add a new element

--- include/lru_cache.hpp
+++ include/lru_cache.hpp
@@ -1,78 +1,84 @@
 #ifndef LRU_CACHE_HPP
 #define LRU_CACHE_HPP
 
-#include <unordered_map>
-#include <list>
-#include <utility>
 #include <cstddef>
-#include <stdexcept>
+#include <list>
 #include <mutex>
+#include <stdexcept>
+#include <unordered_map>
+#include <utility>
 
 namespace caches {
 
-  template <typename Key, typename Value>
-  class lru_cache {
-   public:
-    using value_type = typename std::pair<Key, Value>;
-    using iterator = typename std::list<value_type>::iterator;
-    using operation_guard = typename std::lock_guard<std::mutex>;
+template <typename Key, typename Value>
+class lru_cache {
+ public:
+  using value_type = typename std::pair<Key, Value>;
+  using value_it = typename std::list<value_type>::iterator;
+  using operation_guard = typename std::lock_guard<std::mutex>;
 
-    lru_cache(size_t max_size) : max_cache_size{max_size} {}
-    
-    void Put(const Key& key, const Value& value) {
-      operation_guard og{safe_op};
-      auto it = cache_items_map.find(key);
-      
-      if (it != cache_items_map.end()) {
-        cache_items_list.erase(it->second);
-        cache_items_map.erase(it);
-      }
-      
-      cache_items_list.push_front(std::make_pair(key, value));
-      cache_items_map[key] = cache_items_list.begin();
-      
-      if (cache_items_map.size() > max_cache_size) {
+  lru_cache(size_t max_size) : max_cache_size{max_size} {}
+
+  void Put(const Key& key, const Value& value) {
+    operation_guard og{safe_op};
+    auto it = cache_items_map.find(key);
+
+    if (it != cache_items_map.end()) {
+    }
+
+    if (it == cache_items_map.end()) {
+      if (cache_items_map.size() + 1 > max_cache_size) {
         // remove the last element from cache
-        auto last = cache_items_list.rbegin();
-        
+        auto last = cache_items_list.crbegin();
+
         cache_items_map.erase(last->first);
         cache_items_list.pop_back();
       }
-    }
-    
-    const Value& Get(const Key& key) {
-      operation_guard og{safe_op};
-      auto it = cache_items_map.find(key);
-      
-      if (it == cache_items_map.end()) {
-        throw std::range_error("No such key in the cache");
-      }
-      else {
-        cache_items_list.splice(cache_items_list.begin(), cache_items_list, it->second);
-        
-        return it->second->second;
-      }
-    }
-    
-    bool Exists(const Key& key) const {
-      operation_guard og{safe_op};
-      
-      return cache_items_map.find(key) != cache_items_map.end();
-    }
-    
-    size_t Size() const {
-      operation_guard og{safe_op};
-      
-      return cache_items_map.size();
-    }
-    
-   private:
-    std::list<value_type> cache_items_list;
-    std::unordered_map<Key, iterator> cache_items_map;
-    size_t max_cache_size;
-    mutable std::mutex safe_op;
-  };
 
-} // lru_cache
+      cache_items_list.push_front(std::make_pair(key, value));
+      cache_items_map[key] = cache_items_list.begin();
+    }
+    else {
+      it->second->second = value;
+      cache_items_list.splice(cache_items_list.cbegin(), cache_items_list,
+                              it->second);
+    }
+  }
 
-#endif // LRU_CACHE_HPP
+  const Value& Get(const Key& key) {
+    operation_guard og{safe_op};
+    auto it = cache_items_map.find(key);
+
+    if (it == cache_items_map.end()) {
+      throw std::range_error("No such key in the cache");
+    }
+    else {
+      cache_items_list.splice(cache_items_list.begin(), cache_items_list,
+                              it->second);
+
+      return it->second->second;
+    }
+  }
+
+  bool Exists(const Key& key) const {
+    operation_guard og{safe_op};
+
+    return cache_items_map.find(key) != cache_items_map.end();
+  }
+
+  size_t Size() const {
+    operation_guard og{safe_op};
+
+    return cache_items_map.size();
+  }
+
+ private:
+  std::list<value_type> cache_items_list;
+  std::unordered_map<Key, value_it> cache_items_map;
+  size_t max_cache_size;
+  mutable std::mutex safe_op;
+};
+
+}  // lru_cache
+
+#endif  // LRU_CACHE_HPP

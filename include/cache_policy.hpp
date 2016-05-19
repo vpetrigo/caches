37cd2ffe8acdbdfa7619c5975d079954ab506a2f
Vladimir Petrigo <taenaru@gmail.com>
2016-05-19 08:47:39 +0300

    template for no cache policy

--- include/cache_policy.hpp
+++ include/cache_policy.hpp
@@ -17,6 +17,30 @@ class ICachePolicy {
   // return a key of a displacement candidate
   virtual const Key& DispCandidate() const = 0;
 };
+
+template <typename Key>
+class NoCachePolicy : public ICachePolicy<Key> {
+ public:
+  NoCachePolicy() = default;
+  ~NoCachePolicy() override = default;
+  
+  void Insert(const Key& key) override {
+    
+  }
+  
+  void Touch(const Key& key) override {
+    
+  }
+  
+  void Erase(const Key& key) {
+    
+  }
+
+  // return a key of a displacement candidate
+  virtual const Key& DispCandidate() const {
+    
+  }
+};
 }  // namespace caches
 
 #endif  // CACHE_POLICY_HPP

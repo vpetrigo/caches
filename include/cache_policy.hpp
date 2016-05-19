20ab3a03210003bfcc6f00a58fb2bdfbfeb0e126
Vladimir Petrigo <taenaru@gmail.com>
2016-05-19 18:00:30 +0300

    add NoCachePolicy

--- include/cache_policy.hpp
+++ include/cache_policy.hpp
@@ -1,6 +1,8 @@
 #ifndef CACHE_POLICY_HPP
 #define CACHE_POLICY_HPP
 
+#include <unordered_set>
+
 namespace caches {
 
 template <typename Key>
@@ -14,8 +16,8 @@ class ICachePolicy {
   // handle element deletion from a cache
   virtual void Erase(const Key& key) = 0;
 
-  // return a key of a displacement candidate
-  virtual const Key& DispCandidate() const = 0;
+  // return a key of a replacement candidate
+  virtual const Key& ReplCandidate() const = 0;
 };
 
 template <typename Key>
@@ -23,23 +25,25 @@ class NoCachePolicy : public ICachePolicy<Key> {
  public:
   NoCachePolicy() = default;
   ~NoCachePolicy() override = default;
-  
+
   void Insert(const Key& key) override {
-    
+    key_storage.emplace(key);
   }
-  
+
   void Touch(const Key& key) override {
-    
+    // do not do anything
   }
-  
-  void Erase(const Key& key) {
-    
+
+  void Erase(const Key& key) override {
+    key_storage.erase(key);
   }
 
   // return a key of a displacement candidate
-  virtual const Key& DispCandidate() const {
-    
+  const Key& ReplCandidate() const override {
+    return *key_storage.crbegin();
   }
+ private:
+  std::unordered_set<Key> key_storage;
 };
 }  // namespace caches
 

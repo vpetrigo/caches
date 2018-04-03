dcb9b56721e361012eee1a1df064be009693622b
Mikhail Karasikov <mike-karas@ya.ru>
2018-04-04 00:23:49 +0200

    callback for erased elements, iterators for cached values added

--- include/lru_cache_policy.hpp
+++ include/lru_cache_policy.hpp
@@ -9,19 +9,19 @@ namespace caches {
 template <typename Key>
 class LRUCachePolicy : public ICachePolicy<Key> {
  public:
-  using lru_iterator = typename std::list<Key>::const_iterator;
+  using lru_iterator = typename std::list<Key>::iterator;
 
   LRUCachePolicy() = default;
   ~LRUCachePolicy() = default;
 
   void Insert(const Key& key) override {
     lru_queue.emplace_front(key);
-    key_finder[key] = lru_queue.cbegin();
+    key_finder[key] = lru_queue.begin();
   }
 
   void Touch(const Key& key) override {
     // move the touched element at the beginning of the lru_queue 
-    lru_queue.splice(lru_queue.cbegin(), lru_queue, key_finder[key]);
+    lru_queue.splice(lru_queue.begin(), lru_queue, key_finder[key]);
   }
 
   void Erase(const Key&) override {

e1472782c6b8dd365b613a02bfddd4f8c801e346
Vladimir Petrigo <taenaru@gmail.com>
2018-05-29 11:52:38 +0300

    misc: Clean up code formatting

--- include/cache.hpp
+++ include/cache.hpp
@@ -1,125 +1,151 @@
 #ifndef CACHE_HPP
 #define CACHE_HPP
 
+#include "cache_policy.hpp"
 #include <cstddef>
 #include <limits>
 #include <memory>
 #include <mutex>
 #include <unordered_map>
-#include "cache_policy.hpp"
 
-namespace caches {
+namespace caches
+{
 
 // Base class for caching algorithms
 template <typename Key, typename Value, typename Policy = NoCachePolicy<Key>>
-class fixed_sized_cache {
- public:
-  using iterator = typename std::unordered_map<Key, Value>::iterator;
-  using const_iterator =
-      typename std::unordered_map<Key, Value>::const_iterator;
-  using operation_guard = typename std::lock_guard<std::mutex>;
-  using Callback = typename std::function<void(const Key& key, const Value& value)>;
+class fixed_sized_cache
+{
+  public:
+    using iterator = typename std::unordered_map<Key, Value>::iterator;
+    using const_iterator =
+        typename std::unordered_map<Key, Value>::const_iterator;
+    using operation_guard = typename std::lock_guard<std::mutex>;
+    using Callback =
+        typename std::function<void(const Key &key, const Value &value)>;
 
-  fixed_sized_cache(size_t max_size, const Policy& policy = Policy(),
-                    Callback OnErase = [](const Key&, const Value&) {})
-      : cache_policy(policy), max_cache_size(max_size), OnEraseCallback(OnErase) {
-    if (max_cache_size == 0) {
-      max_cache_size = std::numeric_limits<size_t>::max();
+    fixed_sized_cache(size_t max_size, const Policy &policy = Policy(),
+                      Callback OnErase = [](const Key &, const Value &) {})
+        : cache_policy(policy), max_cache_size(max_size),
+          OnEraseCallback(OnErase)
+    {
+        if (max_cache_size == 0)
+        {
+            max_cache_size = std::numeric_limits<size_t>::max();
+        }
     }
-  }
 
-  ~fixed_sized_cache() { Clear(); }
-
-  void Put(const Key& key, const Value& value) {
-    operation_guard{safe_op};
-    auto elem_it = FindElem(key);
-
-    if (elem_it == cache_items_map.end()) {
-      // add new element to the cache
-      if (Size() + 1 > max_cache_size) {
-        auto disp_candidate_key = cache_policy.ReplCandidate();
-
-        Erase(disp_candidate_key);
-      }
-
-      Insert(key, value);
-    } else {
-      // update previous value
-      Update(key, value);
+    ~fixed_sized_cache()
+    {
+        Clear();
     }
-  }
 
-  const Value& Get(const Key& key) const {
-    operation_guard{safe_op};
-    auto elem_it = FindElem(key);
+    void Put(const Key &key, const Value &value)
+    {
+        operation_guard{safe_op};
+        auto elem_it = FindElem(key);
 
-    if (elem_it == cache_items_map.end()) {
-      throw std::range_error{"No such element in the cache"};
+        if (elem_it == cache_items_map.end())
+        {
+            // add new element to the cache
+            if (Size() + 1 > max_cache_size)
+            {
+                auto disp_candidate_key = cache_policy.ReplCandidate();
+
+                Erase(disp_candidate_key);
+            }
+
+            Insert(key, value);
+        }
+        else
+        {
+            // update previous value
+            Update(key, value);
+        }
     }
-    cache_policy.Touch(key);
 
-    return elem_it->second;
-  }
+    const Value &Get(const Key &key) const
+    {
+        operation_guard{safe_op};
+        auto elem_it = FindElem(key);
 
-  bool Cached(const Key& key) const {
-    operation_guard{safe_op};
-    return FindElem(key) != cache_items_map.end();
-  }
+        if (elem_it == cache_items_map.end())
+        {
+            throw std::range_error{"No such element in the cache"};
+        }
+        cache_policy.Touch(key);
 
-  size_t Size() const {
-    operation_guard{safe_op};
-
-    return cache_items_map.size();
-  }
-
-  void Clear() {
-    operation_guard{safe_op};
-
-    for (auto it = begin(); it != end(); ++it) {
-      cache_policy.Erase(it->first);
-      OnEraseCallback(it->first, it->second);
+        return elem_it->second;
     }
-    cache_items_map.clear();
-  }
 
-  typename std::unordered_map<Key, Value>::const_iterator begin() const {
-    return cache_items_map.begin();
-  }
+    bool Cached(const Key &key) const
+    {
+        operation_guard{safe_op};
+        return FindElem(key) != cache_items_map.end();
+    }
 
-  typename std::unordered_map<Key, Value>::const_iterator end() const {
-    return cache_items_map.end();
-  }
+    size_t Size() const
+    {
+        operation_guard{safe_op};
 
- protected:
-  void Insert(const Key& key, const Value& value) {
-    cache_policy.Insert(key);
-    cache_items_map.emplace(std::make_pair(key, value));
-  }
+        return cache_items_map.size();
+    }
 
-  void Erase(const Key& key) {
-    cache_policy.Erase(key);
+    void Clear()
+    {
+        operation_guard{safe_op};
 
-    auto elem_it = FindElem(key);
-    OnEraseCallback(key, elem_it->second);
-    cache_items_map.erase(elem_it);
-  }
+        for (auto it = begin(); it != end(); ++it)
+        {
+            cache_policy.Erase(it->first);
+            OnEraseCallback(it->first, it->second);
+        }
+        cache_items_map.clear();
+    }
 
-  void Update(const Key& key, const Value& value) {
-    cache_policy.Touch(key);
-    cache_items_map[key] = value;
-  }
+    typename std::unordered_map<Key, Value>::const_iterator begin() const
+    {
+        return cache_items_map.begin();
+    }
 
-  const_iterator FindElem(const Key& key) const {
-    return cache_items_map.find(key);
-  }
+    typename std::unordered_map<Key, Value>::const_iterator end() const
+    {
+        return cache_items_map.end();
+    }
 
- private:
-  std::unordered_map<Key, Value> cache_items_map;
-  mutable Policy cache_policy;
-  mutable std::mutex safe_op;
-  size_t max_cache_size;
-  Callback OnEraseCallback;
+  protected:
+    void Insert(const Key &key, const Value &value)
+    {
+        cache_policy.Insert(key);
+        cache_items_map.emplace(std::make_pair(key, value));
+    }
+
+    void Erase(const Key &key)
+    {
+        cache_policy.Erase(key);
+
+        auto elem_it = FindElem(key);
+        OnEraseCallback(key, elem_it->second);
+        cache_items_map.erase(elem_it);
+    }
+
+    void Update(const Key &key, const Value &value)
+    {
+        cache_policy.Touch(key);
+        cache_items_map[key] = value;
+    }
+
+    const_iterator FindElem(const Key &key) const
+    {
+        return cache_items_map.find(key);
+    }
+
+  private:
+    std::unordered_map<Key, Value> cache_items_map;
+    mutable Policy cache_policy;
+    mutable std::mutex safe_op;
+    size_t max_cache_size;
+    Callback OnEraseCallback;
 };
 }
 
-#endif  // CACHE_HPP
+#endif // CACHE_HPP
--- test/nopolicy_cache_tests.cpp
+++ test/nopolicy_cache_tests.cpp
@@ -4,48 +4,55 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
 
-TEST(NoPolicyCache, Add_one_element) {
-  constexpr std::size_t cache_size = 1;
-  caches::fixed_sized_cache<std::string, int> cache(cache_size);
+TEST(NoPolicyCache, Add_one_element)
+{
+    constexpr std::size_t cache_size = 1;
+    caches::fixed_sized_cache<std::string, int> cache(cache_size);
 
-  cache.Put("Hello", 1);
-  ASSERT_EQ(cache.Get("Hello"), 1);
+    cache.Put("Hello", 1);
+    ASSERT_EQ(cache.Get("Hello"), 1);
 }
 
-TEST(NoPolicyCache, Add_delete_add_one_element) {
-  constexpr std::size_t cache_size = 1;
-  caches::fixed_sized_cache<std::string, int> cache(cache_size);
+TEST(NoPolicyCache, Add_delete_add_one_element)
+{
+    constexpr std::size_t cache_size = 1;
+    caches::fixed_sized_cache<std::string, int> cache(cache_size);
 
-  cache.Put("Hello", 1);
-  cache.Put("World", 2);
-  ASSERT_THROW(cache.Get("Hello"), std::range_error);
-  ASSERT_EQ(cache.Get("World"), 2);
+    cache.Put("Hello", 1);
+    cache.Put("World", 2);
+    ASSERT_THROW(cache.Get("Hello"), std::range_error);
+    ASSERT_EQ(cache.Get("World"), 2);
 }
 
-TEST(NoPolicyCache, Add_many_elements) {
-  constexpr std::size_t cache_size = 1024;
-  caches::fixed_sized_cache<std::string, int> cache(cache_size);
+TEST(NoPolicyCache, Add_many_elements)
+{
+    constexpr std::size_t cache_size = 1024;
+    caches::fixed_sized_cache<std::string, int> cache(cache_size);
 
-  for (std::size_t i = 0; i < cache_size; ++i) {
-    cache.Put(std::to_string(i), i);
-  }
+    for (std::size_t i = 0; i < cache_size; ++i)
+    {
+        cache.Put(std::to_string(i), i);
+    }
 
-  ASSERT_EQ(cache.Size(), cache_size);
+    ASSERT_EQ(cache.Size(), cache_size);
 
-  for (std::size_t i = 0; i < cache_size; ++i) {
-    ASSERT_EQ(cache.Get(std::to_string(i)), i);
-  }
+    for (std::size_t i = 0; i < cache_size; ++i)
+    {
+        ASSERT_EQ(cache.Get(std::to_string(i)), i);
+    }
 }
 
-TEST(NoPolicyCache, Small_cache_many_elements) {
-  constexpr std::size_t cache_size = 1;
-  caches::fixed_sized_cache<std::string, int> cache(cache_size);
+TEST(NoPolicyCache, Small_cache_many_elements)
+{
+    constexpr std::size_t cache_size = 1;
+    caches::fixed_sized_cache<std::string, int> cache(cache_size);
 
-  for (std::size_t i = 0; i < cache_size; ++i) {
-    std::string temp_key = std::to_string(i);
-    cache.Put(temp_key, i);
-    ASSERT_EQ(cache.Get(temp_key), i);
-  }
+    for (std::size_t i = 0; i < cache_size; ++i)
+    {
+        std::string temp_key = std::to_string(i);
+        cache.Put(temp_key, i);
+        ASSERT_EQ(cache.Get(temp_key), i);
+    }
 
-  ASSERT_EQ(cache.Size(), cache_size);
+    ASSERT_EQ(cache.Size(), cache_size);
 }

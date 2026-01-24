369b83e45eb613b8091971fe80b69896cb01b320
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2026-01-24 01:03:08 +0100

    tests: add phmap_flat_hash_map alias to test helper
    - add template alias phmap_flat_hash_map for gtl::flat_hash_map in
      test/test_helper.hpp
    - define alias with template parameters Key, Value, Hash, Equal,
      Allocator to mirror node_hash_map alias

--- test/test_helper.hpp
+++ test/test_helper.hpp
@@ -10,5 +10,8 @@
 template <typename Key, typename Value, typename Hash, typename Equal, typename Allocator>
 using phmap_node_hash_map = gtl::node_hash_map<Key, Value, Hash, Equal, Allocator>;
 
+template <typename Key, typename Value, typename Hash, typename Equal, typename Allocator>
+using phmap_flat_hash_map = gtl::flat_hash_map<Key, Value, Hash, Equal, Allocator>;
+
 template <typename Value>
 using gtl_shared_ptr = gtl::intrusive_ptr<Value>;
